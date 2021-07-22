1611ca7a460eb1c574ed9b48b45b22d21303a0d4
= <=>
2021-07-22 10:18:34 +0700

    finsh

--- src/js/controller.js
+++ src/js/controller.js
@@ -3,7 +3,7 @@ import { MODAL_CLOSE_SEC } from './config.js';
 import recipeView from './views/recipeViews.js';
 import searchViews from './views/searchViews.js';
 import resultViews from './views/resultView.js';
-import paginationView from './views/PaginationView.js';
+import paginationView from './views/paginationView.js';
 import bookmarkView from './views/bookmarkView.js';
 import addRecipeView from './views/addRecipeView.js';
 
